37c8456fb0b9edc12054187c3618fb3cc1d23223
EC2 Default User <ec2-user@ip-172-31-20-19.ec2.internal>
2021-12-08 06:08:13 +0000

    [Fix]header

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   resources :books
 
   devise_for :users
-  resources :users, :only => [:index, :show, :edit]
+  resources :users, :only => [:show, :index, :edit]
 
 end
